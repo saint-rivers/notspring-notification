1a423b715e3bdcc9b5580000d063da958c93b339
eamdayan <eam.dayan@gmail.com>
2022-10-03 16:07:14 +0700

    create nginx deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,7 @@
 docker build . --tag notspring-notification-ui:1.0.0
 
 docker run -itd -p 85:80 notspring-notification-ui:1.0.0
+
+cp nginx/notspring.saintrivers.tech /etc/nginx/conf.d/
+
+certbot --nginx -d notspring.saintrivers.tech
